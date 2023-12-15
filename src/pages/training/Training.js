b4fd017c0d9ff3ec7b00d1097379344d6a415f0f
Nurgazy0986 <n2033853@gmail.com>
2023-12-16 00:07:36 +0600

    newGym

--- src/pages/training/Training.js
+++ src/pages/training/Training.js
@@ -1,6 +1,13 @@
 import React from "react";
+import Button from "../../components/ui/button/Button";
 import topImg from "../../assets/images/line-dec.png";
+import tabsIcon from "../../assets/images/tabs-first-icon.png";
+import triningImg1 from "../../assets/images/training-image-01.jpg";
+import triningImg2 from "../../assets/images/training-image-02.jpg";
+import triningImg3 from "../../assets/images/training-image-03.jpg";
+import triningImg4 from "../../assets/images/training-image-04.jpg";
 
+import "./style/training.css";
 const Training = () => {
   return (
     <div>
@@ -12,7 +19,7 @@ const Training = () => {
               <img src={topImg} alt="" />
               <p>Здесь выберите для себя!</p>
             </div>
-            <div className="training-bottom"></div>
+            <div className="training-bottom">{/* tabs */}</div>
           </div>
         </div>
       </div>
