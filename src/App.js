f9e60195c4a2bce330b59435259dfbd848d26d57
Erlan Zhusupov <erlandevx@gmail.com>
2023-12-16 10:27:07 +0600

    fixed home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
-import Header from "./components/header/Header";
-import Home from "./pages/home/Home";
-import Program from "./pages/program/program";
-import Register from "./pages/register/Register";
-import TeamProfi from "./pages/teamProfi/TeamProfi";
-import Training from "./pages/training/Training";
-import Map from "./pages/map/Map";
+import HomePage from "./pages/homePage/HomePage";
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Program />
-      <Register />
-      <Training />
-      <TeamProfi />
-      <Map />
+      <HomePage />
     </>
   );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,15 @@
 import "./style/home.css";
 import Button from "../../components/ui/button/Button";
+import bgVideo from "../../assets/images/gym-video.mp4";
 
 export default function Header() {
   return (
     <div id="home">
-      {/* <video>
-        <source src="assets/images/gym-video.mp4" type="video/mp4" />
-      </video> */}
+      <div className="hero-div-bg">
+        <video autoplay="autoplay" className="hero-bg">
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+      </div>
       <div className="container">
         <div className="home">
           <div className="center-content text-center ">
